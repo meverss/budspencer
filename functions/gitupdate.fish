9dcbb043f8a207d3e17d1981c285d6ec620811e3
Marvin Eversley Silva <meverss@gmail.com>
2021-06-03 06:42:42 -0400

    Added function

---
+++ functions/gitupdate.fish
@@ -0,0 +1,16 @@
+function gitupdate -d 'Update Git project'
+  set -l branch (command git describe --contains --all HEAD 2> /dev/null )
+  if not test $branch > /dev/null
+    echo 'Este NO es un proyecto Git'
+  else
+    set add (command git add . 2> /dev/null)
+    if test add
+      read -p 'echo Descripción: ' -l desc
+      command git commit -am "$desc"
+      git push -f origin $branch
+      echo; echo 'Proyecto actualuzado'
+    else
+      echo 'El proyecto ya está actualizado'
+    end
+  end
+end
