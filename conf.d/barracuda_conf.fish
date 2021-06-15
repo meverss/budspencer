c8ae636d977638b285e4c1731c090ba4a9a6d737
Marvin Eversley Silva <meverss@gmail.com>
2021-06-15 07:54:27 -0400

    Update files

--- conf.d/barracuda_conf.fish
+++ conf.d/barracuda_conf.fish
@@ -110,9 +110,9 @@ end
 #------------------------------------------------------------------------------
 # Define icons
 #------------------------------------------------------------------------------
-set -U barracuda_icons_dark                                           
-set -U barracuda_icons_light                                           
-#   
+set -U barracuda_icons_dark                                            
+set -U barracuda_icons_light                                            
+#  
 # Set "dark" the default icons scheme
 if not set -q barracuda_icons
   set -U barracuda_icons $barracuda_icons_dark
