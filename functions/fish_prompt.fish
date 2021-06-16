d41573fa272628d7b6d27d0961450f907e034808
Marvin Eversley Silva <meverss@gmail.com>
2021-06-16 02:28:30 -0400

    Update files

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -767,7 +767,7 @@ function __barracuda_prompt_git_branch -d 'Return the current branch name'
             set git_status_info "$git_status_info "(set_color $barracuda_colors[1])"$barracuda_icons[43]$git_behind"
           end
         else
-          set git_status_info $barracuda_icons[44]
+          set git_status_info "$barracuda_icons[44]"
         end    
         echo -n (set_color $barracuda_colors[1])" $barracuda_icons[4] $branch""$git_status_info"' '(set_color $barracuda_colors[3])
       case none
